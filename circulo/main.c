4f312c3bb571472a4d88c57595b282cadf75ef04
Thalisson <thalissonsan9@gmail.com>
2026-03-06 16:07:24 -0300

    Add files via upload

---
+++ circulo/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   double r, area;
+
+   printf("Digite o valor do raio do circulo: ");
+   scanf("%lf", &r);
+
+   area = 3.14159 * (r * r);
+
+   printf("Area: %.3lf\n", area);
+
+
+
+
+
+    return 0;
+}
